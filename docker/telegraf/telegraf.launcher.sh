bd4e426581bb29dfa7f513d69990940490094628
henriklb <bjorseth@kth.se>
2018-07-24 10:34:21 +0200

    Update telegraf.launcher.sh

--- docker/telegraf/telegraf.launcher.sh
+++ docker/telegraf/telegraf.launcher.sh
@@ -2,4 +2,4 @@
 # `/sbin/setuser memcache` runs the given command as the user `memcache`.
 # If you omit that part, the command will be run as root.
 exec /usr/bin/telegraf -config /etc/telegraf/telegraf.conf  >> /var/log/telegraf.log 2>&1
-exec /usr/bin/telegraf -config /etc/telegraf/telegrafsnmpalias.conf  >> /var/log/telegrafsnmpalias.log 2>&1
+exec /usr/bin/telegraf -config /etc/telegraf/telegraf2snmpalias.conf  >> /var/log/telegraf2snmpalias.log 2>&1
